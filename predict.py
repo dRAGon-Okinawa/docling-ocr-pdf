12480a8b2464fadcace6580053e019e1c7e85a34
Arnaud Mengus <amengus@immonot.com>
2025-03-19 14:54:26 +0000

    Enhance prediction functionality by adding input and output format options, and return a list of output files.

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@
 import subprocess
 from cog import BasePredictor, Input, Path
 
+
 class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
@@ -10,22 +11,38 @@ class Predictor(BasePredictor):
     def predict(
         self,
         document: Path = Input(description="Document to process"),
-        timeout: int = Input(description="Maximum time allowed to run the prediction in seconds", default=120, ge=30, le=3600)
-    ) -> Path:
+        from_format: str = Input(
+            description="Input format of the document", default="pdf", choices=["docx", "pptx", "html", "image", "pdf", "asciidoc", "md", "csv", "xlsx", "xml_uspto", "xml_jats", "json_docling"]),
+        to_format: str = Input(
+            description="Output format of the document", default="md", choices=["md", "json", "html", "text", "doctags"]),
+        timeout: int = Input(
+            description="Maximum time allowed to run the prediction in seconds", default=120, ge=30, le=3600)
+    ) -> list[Path]:
         """Run a single prediction on the model"""
-        # Retrieve document name and extension
-        document_name = document.stem
-        # Replace (any) extension to .md :
-        output_document = "/opt/docling/output/" + document_name + ".md"
         print("Docling will work on :", document)
-        print("Docling will output to :", output_document)
+        print("Docling will output to : /opt/docling/output")
 
         # Launch command (system call)
         subprocess.run(
-            ["docling", "--artifacts-path", "/opt/docling/models", "--from", "pdf", "--to", "md", "--image-export-mode", "placeholder", "--table-mode", "accurate", "--output", "/opt/docling/output", document],
-            timeout=timeout,  # Set the timeout in seconds
+            [
+                "docling",
+                "--artifacts-path", "/opt/docling/models",
+                "--from", from_format,
+                "--to", to_format,
+                "--image-export-mode", "placeholder",
+                "--document-timeout", timeout,
+                "--output", "/opt/docling/output",
+                document
+            ],
+            timeout=timeout,  # Command timeout in seconds
             check=True  # Raise exception if the command returns a non-zero exit code
         )
-    
-        # Return the output path
-        return Path(output_document)
+        
+        # Searching for the output files inside the output directory
+        output_files = []
+        for file in Path("/opt/docling/output").iterdir():
+            if file.is_file():
+                output_files.append(Path(file))
+                
+        # Return the output files
+        return output_files
